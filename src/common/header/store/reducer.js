00901af640a4507fc91f97fa1c279334725288af
Zu3zz <zthzththth@163.com>
2019-05-26 20:01:59 +0800

    拆分redux

--- src/common/header/store/reducer.js
+++ src/common/header/store/reducer.js
@@ -1,14 +1,16 @@
+import * as constants from './constants'
+
 const defaultState = {
   focused: false
 }
 export default (state = defaultState, action) => {
-  if (action.type === 'search_focus') {
+  if (action.type === constants.SEARCH_FOCUS) {
     console.log('focuesd is true')
     return {
       focused: true
     }
   }
-  if(action.type === 'search_blur') {
+  if(action.type === constants.SEARCH_BLUR) {
     console.log('focused is false')
     return {
       focused: false
